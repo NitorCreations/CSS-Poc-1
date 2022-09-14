af388ba4f8fc19118ed576d75ab1578cbcd3d81d
jjenqvist <40454881+jjenqvist@users.noreply.github.com>
2022-09-14 14:16:27 +0300

    Formik to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -19,6 +20,57 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Formik, Field, Form, FormikHelpers } from "formik";
+
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="John" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="john@acme.com"
+            type="email"
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+>>>>>>> Stashed changes
     </div>
   );
 }
